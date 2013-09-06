8c2e0fd7263a058a454cfa04ca4b1f3dab1fd8e6
Anders Nierhoff <anders.nierhoff@gmail.com>
2013-09-06 10:25:55 +0200

    adding Shomate equation

--- known_molecules.py
+++ known_molecules.py
@@ -3,12 +3,19 @@ import molecule
 import math
 
 def Gibbs(M,T):
+  #Shoomate equation
+  # solutions found for EtOH, CO2, CO, H2O, N2, O2
+  # link to NIST
+  # #http://webbook.nist.gov/cgi/cbook.cgi?ID=C1333740&Units=SI&Mask=1#Thermo-Gas
+  # this page might be of interest:
+  # http://jkitchin.github.io/blog/tag/thermodynamics/
   Tk=T/1000
   coef = None
   if T<298:
     return False
   if M==H2 and T>=298 and <1000:
-    coef = [33.066178,-11.363417,11.432816,-2.772874,-0.158558,-9.980797,172.707974,0.0]
+    coef = [33.066178,-11.363417,11.432816,-2.772874,-0.158558,-9.980797,172.707974,0.0] #H2
+    
   H=coef[0]*Tk+coef[1]*Tk**2/2+coef[3]*Tk**3/3+coef[4]*Tk**4/4-coef[5]/Tk+coef[6]-coef[8]+M.enthalpy_0
   S=coef[0]*math.log(Tk)+coef[1]*Tk+coef[3]*Tk**2/2+coef[4]*Tk**3/3-coef[5]/(2*Tk**2)+coef[7]
   return H-T*S

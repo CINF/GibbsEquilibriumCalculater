3f9b0d2e49a30aa8528fb29d7a9cc92bbf41d3ea
Robert Jensen <jensen.robert@gmail.com>
2013-09-07 22:05:54 +0200

    It is now possible to check if the reaction is balanced

--- chemical_reaction.py
+++ chemical_reaction.py
@@ -1,14 +1,51 @@
 import atom
 import molecule
+import known_molecules as km
 
 class ChemicalReaction():
     def __init__(self, left, right):
         self.left_side  = left
         self.right_side = right
+    
+    def list_of_atoms(self, side):
+        atoms = {}
+        molecules = {}
+        if side == 'left':
+            molecules = self.left_side
+        if side == 'right':
+            molecules = self.right_side
 
+        atoms = {}
+        for m in molecules:
+            elements = m.list_of_atoms()
+            for element in elements.keys():
+                if element in atoms:
+                    atoms[element] += elements[element]
+                else:
+                    atoms[element] = elements[element]
+        return atoms
+    
+    def is_balanced(self):
+        left  = self.list_of_atoms('left')
+        right = self.list_of_atoms('right')
+        return left == right
+        
 if __name__ == '__main__':
 
-    CO = molecule.Molecule([atom.Atom(6), atom.Atom(8)])
-    CO2 = molecule.Molecule([atom.Atom(6), atom.Atom(8), atom.Atom(8)])
-    
-    reaction = ChemicalReaction([CO], [CO2])
+    reaction = ChemicalReaction([km.CO,km.O2,km.CO2], [km.CO2])
+    print reaction.list_of_atoms('left')
+    print reaction.list_of_atoms('right')
+
+    print reaction.is_balanced()
+    """
+    left_atoms = {}
+    for mol in reaction.left_side:
+        elements = mol.list_of_atoms()
+        for element in elements.keys():
+            if element in left_atoms:
+                left_atoms[element] = left_atoms[element] + elements[element]
+            else:
+                left_atoms[element] = elements[element]
+
+    print left_atoms
+    """
